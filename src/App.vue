6fb4125e3217cba3cf940c354745937667624fb5
Magic <316063522@qq.com>
2019-04-26 15:49:24 +0800

    feat:补充内容

--- src/App.vue
+++ src/App.vue
@@ -25,10 +25,8 @@
       // https://bootswatch.com/ 使用bootstrap样式
       // https://tech.yandex.com/ 注册获取key调用接口实习翻译
       translateText(text,language){
-        console.log(text,language)
         this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190426T031856Z.18b44da5843e4c2e.28fd16c9143bf47bb79868a1c9473163799754e4&lang='+language+'&text='+text)
           .then(resp => {
-            console.log(resp)
             this.translatedText = resp.body.text[0]
           })
       }
